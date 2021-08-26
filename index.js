38616e79aca2f58311e2d533472161daa9f93232
Fernando Flores <nbafernandoflores8@gmail.com>
2021-08-26 13:01:38 -0600

    JS

--- index.js
+++ index.js
@@ -20,22 +20,23 @@ var inicial = ["B", "DIRTY", "CLEAN"];
 
 
 function obtenerEstado(numEstado) {
-  if (numEstado == 1) {
-    return window.document.getElementById("primero").innerHTML = primero;
-  } else if (numEstado == 2) {
-    return window.document.getElementById("segundo").innerHTML = segundo;
-  } else if (numEstado == 3) {
-    return window.document.getElementById("tercero").innerHTML = tercero;
-  } else if (numEstado == 4) {
-    return window.document.getElementById("cuarto").innerHTML = cuarto;
-  } else if (numEstado == 5) {
-    return window.document.getElementById("quinto").innerHTML = quinto;
-  } else if (numEstado == 6) {
-    return window.document.getElementById("sexto").innerHTML = sexto;
-  } else if (numEstado == 7) {
-    return window.document.getElementById("septimo").innerHTML = septimo;
-  } else if (numEstado == 8) {
-    return window.document.getElementById("octavo").innerHTML = octavo;
+  switch (numEstado) {
+    case 1:
+      return window.document.getElementById("primero").innerHTML = primero;
+    case 2:
+      return window.document.getElementById("segundo").innerHTML = segundo;
+    case 3:
+      return window.document.getElementById("tercero").innerHTML = tercero;
+    case 4:
+      return window.document.getElementById("cuarto").innerHTML = cuarto;
+    case 5:
+      return window.document.getElementById("quinto").innerHTML = quinto;
+    case 6:
+      return window.document.getElementById("sexto").innerHTML = sexto;
+    case 7:
+      return window.document.getElementById("septimo").innerHTML = septimo;
+    case 8:
+      return window.document.getElementById("octavo").innerHTML = octavo;
   }
 }
 
@@ -75,19 +76,18 @@ function plot(arrayEstados) {
 
 function start(inicial) {
   var location = inicial[0];
-  var state = inicial[0] == "A" ? inicial[1] : inicial[2];
+  var state = inicial[0] === "A" ? inicial[1] : inicial[2];
   var action_result = reflex_agent(location, state);
   let child = document.createElement("p");
   child.innerHTML = `Estado: ${location} | Action ${action_result}`;
   //document.getElementById("log").prepend(child);
   document.getElementById('log').value += "\n"+child.innerText;
-  if (action_result == "CLEAN") {
-    if (location == "A") inicial[1] = "CLEAN";
-    else if (location == "B") inicial[2] = "CLEAN";
-  } else if (action_result == "RIGHT") inicial[0] = "B";
-  else if (action_result == "LEFT") inicial[0] = "A";
+  if (action_result === "CLEAN") if (location === "A") inicial[1] = "CLEAN";
+  else if (location === "B") inicial[2] = "CLEAN";
+  else if (action_result === "RIGHT") inicial[0] = "B";
+  else if (action_result === "LEFT") inicial[0] = "A";
   if (primero >= 2 && segundo >= 2 && tercero >= 2 && cuarto >= 2 && quinto >= 2 && sexto >= 2 && septimo >= 2 && octavo >= 2) {
-    alert("CICLO FINALIZADO, ESTADOS VISITADOS !!! ");
+    alert("SE HAN VISITADO TODOS LOS ESTADOS");
     return;
   }
   setTimeout(function () {
@@ -97,7 +97,7 @@ function start(inicial) {
 }
 
 function random(inicial) {
-  if (inicial[1] == "CLEAN" && Math.floor(Math.random() * 10) > 7) {
+  if (inicial[1] === "CLEAN" && Math.floor(Math.random() * 10) > 7) {
     inicial[1] = "DIRTY";
     document.getElementById("ABasura").classList.remove("d-none");
     let child = document.createElement("p");
@@ -105,7 +105,7 @@ function random(inicial) {
     document.getElementById('log').value += "\n"+child.innerText;
     //document.getElementById("log").prepend(child);
   }
-  if (inicial[2] == "CLEAN" && Math.floor(Math.random() * 10) < 5) {
+  if (inicial[2] === "CLEAN" && Math.floor(Math.random() * 10) < 5) {
     inicial[2] = "DIRTY";
     document.getElementById("BBasura").classList.remove("d-none");
     let child = document.createElement("p");
@@ -117,18 +117,18 @@ function random(inicial) {
 }
 
 function reflex_agent(location, state) {
-  if (state == "DIRTY") {
-    if (location == "A") {
+  if (state === "DIRTY") {
+    if (location === "A") {
       document.getElementById("ABasura").classList.add("d-none");
     } else {
       document.getElementById("BBasura").classList.add("d-none");
     }
     return "CLEAN";
-  } else if (location == "A") {
+  } else if (location === "A") {
     document.getElementById("BAspi").classList.remove("d-none");
     document.getElementById("AAspi").classList.add("d-none");
     return "RIGHT";
-  } else if (location == "B") {
+  } else if (location === "B") {
     document.getElementById("AAspi").classList.remove("d-none");
     document.getElementById("BAspi").classList.add("d-none");
     return "LEFT";
